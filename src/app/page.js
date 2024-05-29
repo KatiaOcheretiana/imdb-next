e99af4a2d3fd56b777c9453fe4f01b5552a1d71d
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-05-29 21:25:35 +0300

    fetch data from imdb

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,30 @@
+import axios from "axios";
 import React from "react";
+import Results from "../components/Results";
 
-function Home() {
-  return <div>Home</div>;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=663f98b62f35cf0441bf8b451b47f044&language=en-US&page=1`
+  );
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("failed to fetch data");
+  }
+
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
 
 export default Home;
---
+++ src/components/Results.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Results({ results }) {
+  return (
+    <ul>
+      {results.map((el) => (
+        <li key={el.id}>
+          <Image
+            alt={el.original_title}
+            src={`https://image.tmdb.org/t/p/w500${el.poster_path}`}
+            width="200"
+            height={"100"}
+          />
+          <p>{el.original_title}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
