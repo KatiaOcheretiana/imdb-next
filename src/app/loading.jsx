15fd8383d84573473e3b17318ad72651ec2d484d
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-05-30 10:47:08 +0300

    add the loading

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="flex justify-center pt-20">
+      <img src="loader.svg" alt="Loading.." />
+    </div>
+  );
+}
