55a408d120e9f429bfe6bf0b8a44d221eb213713
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-05-30 21:58:26 +0300

    Update NavBarItem.jsx

--- src/components/NavBarItem.jsx
+++ src/components/NavBarItem.jsx
@@ -2,22 +2,24 @@
 
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
 export default function NavBarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
 
   return (
-    <Link
-      className={`hover:text-amber-600 font-semibold ${
-        genre === param
-          ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-          : ""
-      }`}
-      href={`/?genre=${param}`}
-    >
-      {title}
-    </Link>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param
+            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            : ""
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </Suspense>
   );
 }
